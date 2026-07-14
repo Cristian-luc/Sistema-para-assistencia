26453f8cd3928cf093755f610e7e03f46703a0ea
Cristian Moura <cristianbaixista@gmail.com>
2026-07-14 14:45:28 +0000

    14/07

--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Clientes
+    
+def listar_clientes(request):
+    clientes = Clientes.object.all()
 
-def criar_clientes(request):
-    
-    
+    return render (request, "clientes/lista_clientes.html") 
+    {'clientes' : Clientes}
+
 
