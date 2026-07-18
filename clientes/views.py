4ecd0914c8f0c89238ecf9ac2494ee006c6e153f
Cristian Moura <cristianbaixista@gmail.com>
2026-07-18 17:46:23 +0000

    clientes

--- clientes/views.py
+++ clientes/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .formys import ClienteForm
     
 def index(request):
     return render (request, "index.html") 
+
+# Criar
+def criar_cliente(request):
+    if request.method == "POST":
+        form = ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("lista_clientes")
+    else:
+        form = ClienteForm()
+
+    return render(request, "clientes.html", {
+        "form": form
+    })
+
     
 
 
